a487162d33197f90a173e32b2087695c4ca17173
m-nazish <nazish@binmile.com>
2019-11-11 16:22:08 +0530

    IG 103
    The price list isue for opportunity

--- ImproveGroup/ImproveGroup/OpportunityPriceList.cs
+++ ImproveGroup/ImproveGroup/OpportunityPriceList.cs
@@ -6,13 +6,17 @@ namespace ImproveGroup
 {
     public class OpportunityPriceList : IPlugin
     {
+        ITracingService tracingService;
+        IPluginExecutionContext context;
+        IOrganizationServiceFactory serviceFactory;
+        IOrganizationService service;
         public void Execute(IServiceProvider serviceProvider)
         {
             #region Setup
-            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
-            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
-            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
-            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+            tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            service = serviceFactory.CreateOrganizationService(context.UserId);
             #endregion
 
             if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
@@ -30,37 +34,41 @@ namespace ImproveGroup
                         var currencyId = (Guid)transactionCurrency.Id;
 
                         Entity priceList = new Entity("pricelevel");
-                        priceList["name"] = entity.Attributes["name"]+entity.Id.ToString();
+                        priceList["name"] = entity.Attributes["name"] + entity.Id.ToString();
                         priceList["transactioncurrencyid"] = new EntityReference(transactionCurrency.LogicalName, currencyId);
-                        service.Create(priceList);
-                    }
-                    if (entity.Attributes.Contains("pricelevelid"))
-                    {
-                        var priceListId = new Guid();
-                        var fetchData = new
-                        {
-                            name = entity.Attributes["name"] + entity.Id.ToString()
-                        };
-                        var fetchXml = $@"
-                                        <fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
-                                            <entity name='pricelevel'>
-                                            <attribute name='name' />
-                                            <attribute name='pricelevelid' />
-                                            <filter type='and'>
-                                                <condition attribute='name' operator='eq' value='{fetchData.name}'/>
-                                            </filter>
-                                            </entity>
-                                        </fetch>";
-                        EntityCollection result = service.RetrieveMultiple(new FetchExpression(fetchXml));
-                        foreach (var priceList in result.Entities)
-                        {
-                            priceListId = (Guid)priceList.Attributes["pricelevelid"];
-                            entity["pricelevelid"] = new EntityReference(priceList.LogicalName, priceList.Id);
-                            service.Update(entity);
+                        //service.Create(priceList);
 
-                        }
-
+                        //Changes made to fix the price list issues...
+                        var priceListId = service.Create(priceList);
+                        UpdateOpportunityPriceList(priceListId, entity.Id);
                     }
+                    //if (entity.Attributes.Contains("pricelevelid"))
+                    //{
+                    //    var priceListId = new Guid();
+                    //    var fetchData = new
+                    //    {
+                    //        name = entity.Attributes["name"] + entity.Id.ToString()
+                    //    };
+                    //    var fetchXml = $@"
+                    //                    <fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+                    //                        <entity name='pricelevel'>
+                    //                        <attribute name='name' />
+                    //                        <attribute name='pricelevelid' />
+                    //                        <filter type='and'>
+                    //                            <condition attribute='name' operator='eq' value='{fetchData.name}'/>
+                    //                        </filter>
+                    //                        </entity>
+                    //                    </fetch>";
+                    //    EntityCollection result = service.RetrieveMultiple(new FetchExpression(fetchXml));
+                    //    foreach (var priceList in result.Entities)
+                    //    {
+                    //        priceListId = (Guid)priceList.Attributes["pricelevelid"];
+                    //        entity["pricelevelid"] = new EntityReference(priceList.LogicalName, priceList.Id);
+                    //        service.Update(entity);
+
+                    //    }
+
+                    //}
                 }
                 catch (Exception ex)
                 {
@@ -71,6 +79,19 @@ namespace ImproveGroup
                     service.Create(errorLog);
                     throw;
                 }
+            }
+        }
+        protected void UpdateOpportunityPriceList(Guid priceListId, Guid opportunityId)
+        {
+            try
+            {
+                Entity entity = service.Retrieve("opportunity", opportunityId, new ColumnSet("pricelevelid"));
+                entity["pricelevelid"] = new EntityReference("pricelevel", priceListId);
+                service.Update(entity);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
             }
         }
     }
