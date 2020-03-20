9bfe981bd32b1a5657d73e33736f75f11f33bd90
m-nazish <55585286+m-nazish@users.noreply.github.com>
2020-03-20 10:31:05 +0530

    IG 116

--- ImproveGroup/IG_CalculateTax/Controllers/HomeController.cs
+++ ImproveGroup/IG_CalculateTax/Controllers/HomeController.cs
@@ -12,77 +12,56 @@ namespace IG_CalculateTax.Controllers
     public class HomeController : Controller
     {
         [System.Web.Http.HttpGet]
-        [EnableCors(origins: "https://igcalculatetax.azurewebsites.net/", headers: "*", methods: "*")]
-        public void Index()
+        [EnableCors(origins: "https://sandbox-rest.avatax.com", headers: "*", methods: "*")]
+        public string Index()
         {
-            var client = new AvaTaxClient("Dynamics", "9.1", Environment.MachineName, AvaTaxEnvironment.Production).WithSecurity("gordonklein@improvegroup.net", "Impr@ve.3550");
-
-            // Setup transaction model
-            var createModel = new CreateTransactionModel()
+            string totalTax= string.Empty;
+            try
             {
-                type = DocumentType.SalesOrder,
-                companyCode = "DEMOPAGE",
-                date = DateTime.Today,
-                customerCode = "ABC",
-                lines = new List<LineItemModel>()
-    {
-        new LineItemModel()
-        {
-            number = "1",
-            quantity = 1,
-            amount = 40.21m,
-            taxCode = "PC040400"
-        }
-    },
-                addresses = new AddressesModel()
-                {
-                    singleLocation = new AddressLocationInfo()
-                    {
-                        line1 = "2000 Main Street",
-                        city = "Irvine",
-                        region = "CA",
-                        country = "US",
-                        postalCode = "92614"
-                    }
-                }
-            };
-            // Create transaction
-            var transaction = client.CreateTransaction(null, createModel);
-        }
-        public void GetData()
-        {
-            var client = new AvaTaxClient("Dynamics", "9.1", Environment.MachineName, AvaTaxEnvironment.Sandbox)
-    .WithSecurity("gordonklein@improvegroup.net", "3550.Impr@ve");
+                var client = new AvaTaxClient("Dynamics", "9.1", Environment.MachineName, AvaTaxEnvironment.Sandbox)
+            .WithSecurity("gordonklein@improvegroup.net", "3550.Impr@ve");
+                //Ship From Address:
+                string shipFromLine1 = "3550 Pan American Freeway";
+                string shipFromLine2 = null;
+                string shipFromLine3 = null;
+                string shipFromCity = "Albuquerque";
+                string shipFromRegion = "NM";
+                string shipFromPostalCode = "87107";
+                string shipFromCountry = "USA";
 
-            // Setup transaction model
-            var createModel = new CreateTransactionModel()
+                //Ship To Address:
+                string shipToLine1 = "2137 Birchwood Dr";
+                string shipToLine2 = null;
+                string shipToLine3 = null;
+                string shipToCity = "San Antonio";
+                string shipToRegion = "TX";
+                string shipToPostalCode = "78236";
+                string shipToCountry = "USA";
+
+                decimal frAmount = 10000;
+                decimal line1Amount = 4104;
+                decimal Line2Amount = 10000;
+                var transaction1 = new TransactionBuilder(client, "IMPROVEGROUPINC", DocumentType.SalesOrder, "824859ec-efce-e911-a95e-000d3a110bbd")
+                           .WithAddress(TransactionAddressType.ShipFrom, "3550 Pan American Freeway", null, null, "Albuquerque", "NM", "87107", "USA")
+                           .WithAddress(TransactionAddressType.ShipTo, "100 Ravine Lane NE", null, null, "Bainbridge Island", "WA", "98110", "US")
+                           .WithLine(100.0m)
+                           .WithLine(1234.56m) // Each line is added as a separate item on the invoice!
+                           .WithExemptLine(50.0m, "NT") // An exempt item!
+                           .WithLine(2000.0m) // The two addresses below apply to this $2000 line item
+                           .WithLineAddress(TransactionAddressType.ShipFrom, shipFromLine1, shipFromLine2, shipFromLine3, shipFromCity, shipFromRegion, shipFromPostalCode, shipFromCountry)
+                           .WithLineAddress(TransactionAddressType.ShipTo, shipToLine1, shipToLine2, shipToLine3, shipToCity, shipToRegion, shipToPostalCode, shipToCountry)
+                           .WithLine(line1Amount, 1, "SP156226", "PM Labor", "PML001")
+                           .WithLine(Line2Amount, 1, "P0000000", "Ban Air Cargo Rack", "0065")
+                           .WithLine(frAmount, taxCode: "FR010000") // shipping costs!
+                           .Create();
+                totalTax =Convert.ToString(transaction1.totalTax);
+            }
+            catch (Exception ex)
             {
-                type = DocumentType.SalesOrder,
-                companyCode = "IMPROVEGROUPINC",
-                date = DateTime.Today,
-                customerCode = "65e8b975-15cf-e911-a96a-000d3a1d55a5",
-                lines = new List<LineItemModel>()
-    {
-        new LineItemModel()
-        {
-            number = "1",
-            quantity = 1,
-            amount = 40.21m,
-            taxCode = "PC040400"
-        }
-    },
-                addresses = new AddressesModel()
-                {
-                    singleLocation = new AddressLocationInfo()
-                    {
-                        line1 = "2000 Main Street",
-                        city = "Irvine",
-                        region = "CA",
-                        country = "USA",
-                        postalCode = "92614"
-                    }
-                }
-            };
+
+            }
+
+            return totalTax;
         }
     }
 }
