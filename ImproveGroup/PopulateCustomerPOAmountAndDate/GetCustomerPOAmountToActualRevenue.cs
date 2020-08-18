1eb9d24e1cb65153853cdadc4471a7629b76f59f
snehbinmile <65010773+snehbinmile@users.noreply.github.com>
2020-08-18 11:55:18 +0530

    IG 134
    Below plugins have been added...
    
    1. UpdateProjectManagerInAAR.
    2. UpdateQuoteAmountForOrderEntry.
    3.IG_UpdateFinanceTabValues.
    4.PopulateCustomerPOAmountAndDate.

---
+++ ImproveGroup/PopulateCustomerPOAmountAndDate/GetCustomerPOAmountToActualRevenue.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace PopulateCustomerPOAmountAndDate
+{
+    public class GetCustomerPOAmountToActualRevenue : IPlugin
+    {
+        IPluginExecutionContext context;
+        IOrganizationServiceFactory serviceFactory;
+        IOrganizationService service;
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            try
+            {
+
+                context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+                serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                service = serviceFactory.CreateOrganizationService(null);
+
+                if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+                {
+                    
+                    Guid opportunityId = Guid.Empty;
+                    Entity entity = (Entity)context.InputParameters["Target"];
+                    if (entity.LogicalName != "opportunityclose")
+                    {
+                        return;
+                    }
+
+                    Entity opportunityClose = service.Retrieve("opportunityclose", entity.Id, new ColumnSet("opportunityid"));
+                    if (opportunityClose.Attributes.Contains("opportunityid") && opportunityClose.Attributes["opportunityid"]!=null)
+                    {
+                        EntityReference opportunity = (EntityReference)opportunityClose.Attributes["opportunityid"];
+                        opportunityId = opportunity.Id;
+                    }
+
+                    UpdateOppClose(opportunityId, opportunityClose);
+
+                }
+            }
+
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        protected void UpdateOppClose(Guid opportunityid, Entity opportunityClose)
+        {
+            var fetchData = new
+            {
+                opportunityid = opportunityid
+            };
+            var fetchXml = $@"
+                                     <fetch>
+                                     <entity name='opportunity'>
+                                     <attribute name='ig1_customerpodate' />
+                                     <attribute name='ig1_customerpoamount' />
+                                     <filter type='and'>
+                                     <condition attribute='opportunityid' operator='eq' value='{fetchData.opportunityid}'/>
+                                     </filter>
+                                     </entity>
+                                     </fetch>";
+            var result = service.RetrieveMultiple(new FetchExpression(fetchXml));
+
+            
+            if (result.Entities.Count>0)
+            {
+                var record = result.Entities[0].Attributes;
+                
+                if (record.Contains("ig1_customerpoamount") && record["ig1_customerpoamount"] !=null)
+                {
+                    Money money = (Money)record["ig1_customerpoamount"];
+
+                    opportunityClose.Attributes["actualrevenue"] = money;
+                }
+                if (record.Contains("ig1_customerpodate") && record["ig1_customerpodate"]!=null)
+                {
+                    opportunityClose.Attributes["actualend"] = Convert.ToDateTime(record["ig1_customerpodate"]);
+                }
+                service.Update(opportunityClose);      
+            }
+        }
+    }
+}
---
+++ ImproveGroup/UpdateQuoteAmountForOrderEntry/Updatequoteamountfromquote.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace UpdateQuoteAmountForOrderEntry
+{
+
+    public class Updatequoteamountfromquote : IPlugin
+    {
+        IPluginExecutionContext context;
+        IOrganizationServiceFactory serviceFactory;
+        IOrganizationService service;
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+
+            context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            service = serviceFactory.CreateOrganizationService(null);
+
+            try
+            {
+                   if (context.MessageName.ToLower() == "delete")
+                     {
+                    if (context.PreEntityImages.Contains("Image"))
+                    {
+                        Entity entity = (Entity)context.PreEntityImages["Image"];
+
+                        if (entity.LogicalName == "quote")
+                           {
+
+                            EntityReference er = (EntityReference)entity.Attributes["opportunityid"];
+                            Guid oppid = er.Id;
+                            GetActivatedquoteamount(oppid);
+                                   }
+                           }
+                    }
+
+                   else if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+                     {
+
+
+                    Entity entity = (Entity)context.InputParameters["Target"];
+
+                    string entityName = entity.LogicalName;
+
+                    if (entityName == "quote")
+                    {
+                        Entity fetchoppid = service.Retrieve(entityName, entity.Id, new ColumnSet("opportunityid"));
+                        if (fetchoppid.Attributes.Contains("opportunityid") && fetchoppid.Attributes["opportunityid"] != null)
+                        {
+
+                            Guid oppid = fetchoppid.GetAttributeValue<EntityReference>("opportunityid").Id;
+
+                            GetActivatedquoteamount(oppid);
+
+                        }
+                    }
+
+                }
+
+
+            }
+
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+
+        public void GetActivatedquoteamount(Guid id)
+        {
+
+            var fetchData = new
+            {
+                opportunityid = id,
+                statecode = "2",
+                statecode2 = "1"
+            };
+            var fetchXml = $@"
+                         <fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+                                 <entity name='quote'>
+                                 <attribute name='statecode' />
+                                 <attribute name='totalamount' />
+                                 <filter type='and'>
+                                <condition attribute='opportunityid' operator='eq' value='{fetchData.opportunityid/*b59ae3b0-3fcd-407c-83b6-a69c1189be2e*/}'/>
+                                <filter type='or'>
+                                <condition attribute='statecode' operator='eq' value='{fetchData.statecode/*2*/}'/>
+                              <condition attribute='statecode' operator='eq' value='{fetchData.statecode2/*1*/}'/>
+                        </filter>
+                       </filter>
+                       </entity>
+                       </fetch>";
+
+
+            EntityCollection result = service.RetrieveMultiple(new FetchExpression(fetchXml));
+            decimal calculateamount = 0;
+            Entity entobj = new Entity("opportunity", id);
+            if (result.Entities.Count>0 && result.Entities != null)
+              {
+ 
+                for(int i=0;i< result.Entities.Count; i++)
+                {
+
+                    Money totalamount = new Money(0);
+
+                    totalamount = result.Entities[i].GetAttributeValue<Money>("totalamount");
+                    calculateamount = calculateamount + Convert.ToDecimal(totalamount.Value);
+                }
+
+
+               
+                entobj["ig1_totalamountfromquote"] = new Money(calculateamount);
+                service.Update(entobj);      
+            }
+            else
+            {
+                entobj["ig1_totalamountfromquote"] = new Money(0);
+                service.Update(entobj);
+            }
+        }
+    }
+}
