be790707100d4c0064e1ebf77aab0764360c49d7
Mohd Nazish <nazish@binmile.com>
2020-10-21 19:22:59 +0530

    IG 156

--- ImproveGroup/IG_DeleteBidSheet/DeleteBidSheet.cs
+++ ImproveGroup/IG_DeleteBidSheet/DeleteBidSheet.cs
@@ -17,41 +17,180 @@ namespace IG_DeleteBidSheet
                 serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
                 service = serviceFactory.CreateOrganizationService(null);
 
-                if (context.InputParameters.Equals(null) || string.IsNullOrEmpty(context.InputParameters["recordId"].ToString()))
+                if (context.InputParameters.Equals(null) || !context.InputParameters.Contains("recordId") || string.IsNullOrEmpty(context.InputParameters["recordId"].ToString()))
                 {
                     return;
                 }
+                bool lineItemsDeleted = false;
+                bool indirectCostDeleted = false;
+                bool bsProductsDeleted = false;
+                bool categoriesDeleted = false;
 
-                Guid recordId = new Guid(context.InputParameters["recordId"].ToString());
-                if (recordId == Guid.Empty)
+                Guid recordid = new Guid(context.InputParameters["recordId"].ToString());
+                if (recordid != Guid.Empty)
                 {
-                    return;
+                    lineItemsDeleted = BidSheetLineItems(recordid);
+                    indirectCostDeleted = IndirectCosts(recordid);
+                    bsProductsDeleted = BidsheetProducts(recordid);
+                    categoriesDeleted = Categories(recordid);
+                    if (lineItemsDeleted && indirectCostDeleted && bsProductsDeleted && categoriesDeleted)
+                    {
+                        service.Delete("ig1_bidsheet", recordid);
+                    }
                 }
-                string[] entities = { "ig1_bidsheetpricelistitem", "ig1_associatedcost", "ig1_bidsheetproduct", "ig1_bscategoryvendor" };
-                for (int i = 0; i < entities.Length; i++)
-                {
-                    DeleteAssociatedRecords(recordId, entities[i]);
-                }
-                service.Delete("ig1_bidsheet", recordId);
             }
             catch (Exception ex)
             {
-                var trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
-                trace.Trace("Throwing DeleteBidSheet Plugin");
                 throw new InvalidPluginExecutionException("Error " + ex);
             }
         }
-        protected void DeleteAssociatedRecords(Guid recordId, string entityname)
+        protected bool BidSheetLineItems(Guid recordid)
         {
-            QueryByAttribute query = new QueryByAttribute(entityname);
-            query.ColumnSet = new ColumnSet(true);
-            query.Attributes.AddRange("ig1_bidsheet");
-            query.Values.AddRange(recordId);
-            EntityCollection ec = service.RetrieveMultiple(query);
-            foreach (var rec in ec.Entities)
+            bool bidsheetlineItemsDeleted = false;
+            var fetchData = new
             {
-                service.Delete(entityname, rec.Id);
+                ig1_bidsheet = recordid
+            };
+            var fetchXml = $@"
+                            <fetch>
+                              <entity name='ig1_bidsheetpricelistitem'>
+                                <attribute name='ig1_product' />
+                                <filter type='and'>
+                                  <condition attribute='ig1_bidsheet' operator='eq' value='{fetchData.ig1_bidsheet/*d9c97819-7813-eb11-a813-000d3a98d1ad*/}'/>
+                                </filter>
+                              </entity>
+                            </fetch>";
+            EntityCollection entityCollection = service.RetrieveMultiple(new FetchExpression(fetchXml));
+            if (entityCollection.Entities.Count > 0)
+            {
+                int count = 0;
+                int totalRecords = entityCollection.Entities.Count;
+                foreach (var item in entityCollection.Entities)
+                {
+                    service.Delete(item.LogicalName, item.Id);
+                    count++;
+                }
+                if (totalRecords == count)
+                {
+                    bidsheetlineItemsDeleted = true;
+                }
             }
+            else
+            {
+                bidsheetlineItemsDeleted = true;
+            }
+            return bidsheetlineItemsDeleted;
+        }
+        protected bool IndirectCosts(Guid recordid)
+        {
+            bool indirectCostsDeleted = false;
+            var fetchData = new
+            {
+                ig1_bidsheet = recordid
+            };
+            var fetchXml = $@"
+                            <fetch>
+                                <entity name='ig1_associatedcost'>
+                                <attribute name='ig1_bidsheetcategory' />
+                                <filter type='and'>
+                                    <condition attribute='ig1_bidsheet' operator='eq' value='{fetchData.ig1_bidsheet/*d9c97819-7813-eb11-a813-000d3a98d1ad*/}'/>
+                                </filter>
+                                </entity>
+                            </fetch>";
+            EntityCollection entityCollection = service.RetrieveMultiple(new FetchExpression(fetchXml));
+            if (entityCollection.Entities.Count > 0)
+            {
+                int count = 0;
+                int totalRecords = entityCollection.Entities.Count;
+                foreach (var item in entityCollection.Entities)
+                {
+                    service.Delete(item.LogicalName, item.Id);
+                    count++;
+                }
+                if (totalRecords == count)
+                {
+                    indirectCostsDeleted = true;
+                }
+            }
+            else
+            {
+                indirectCostsDeleted = true;
+            }
+            return indirectCostsDeleted;
+        }
+        protected bool BidsheetProducts(Guid recordid)
+        {
+            bool bsProductsDeleted = false;
+            var fetchData = new
+            {
+                ig1_bidsheet = recordid
+            };
+            var fetchXml = $@"
+                            <fetch>
+                              <entity name='ig1_bidsheetproduct'>
+                                <attribute name='ig1_name' />
+                                <filter type='and'>
+                                  <condition attribute='ig1_bidsheet' operator='eq' value='{fetchData.ig1_bidsheet/*d9c97819-7813-eb11-a813-000d3a98d1ad*/}'/>
+                                </filter>
+                              </entity>
+                            </fetch>";
+            EntityCollection entityCollection = service.RetrieveMultiple(new FetchExpression(fetchXml));
+            if (entityCollection.Entities.Count > 0)
+            {
+                int count = 0;
+                int totalRecords = entityCollection.Entities.Count;
+                foreach (var item in entityCollection.Entities)
+                {
+                    service.Delete(item.LogicalName, item.Id);
+                    count++;
+                }
+                if (count == totalRecords)
+                {
+                    bsProductsDeleted = true;
+                }
+            }
+            else
+            {
+                bsProductsDeleted = true;
+            }
+            return bsProductsDeleted;
+        }
+        protected bool Categories(Guid recordid)
+        {
+            bool categoriesDeleted = false;
+            var fetchData = new
+            {
+                ig1_bidsheet = recordid
+            };
+            var fetchXml = $@"
+                            <fetch>
+                              <entity name='ig1_bscategoryvendor'>
+                                <attribute name='ig1_category' />
+                                <filter type='and'>
+                                  <condition attribute='ig1_bidsheet' operator='eq' value='{fetchData.ig1_bidsheet/*ig1_bidsheet*/}'/>
+                                </filter>
+                              </entity>
+                            </fetch>";
+            EntityCollection entityCollection = service.RetrieveMultiple(new FetchExpression(fetchXml));
+            if (entityCollection.Entities.Count > 0)
+            {
+                int count = 0;
+                int totalRecords = entityCollection.Entities.Count;
+                foreach (var item in entityCollection.Entities)
+                {
+                    service.Delete(item.LogicalName, item.Id);
+                    count++;
+                }
+                if (count == totalRecords)
+                {
+                    categoriesDeleted = true;
+                }
+            }
+            else
+            {
+                categoriesDeleted = true;
+            }
+            return categoriesDeleted;
         }
     }
 }
