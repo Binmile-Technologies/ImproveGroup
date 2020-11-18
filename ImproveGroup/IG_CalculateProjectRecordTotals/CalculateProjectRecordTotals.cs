3d6e5bc7daa73e17506516796707d3db85bbccc6
Mohd Nazish <nazish@binmile.com>
2020-11-18 10:43:46 +0530

    IG 166

--- ImproveGroup/IG_CalculateProjectRecordTotals/CalculateProjectRecordTotals.cs
+++ ImproveGroup/IG_CalculateProjectRecordTotals/CalculateProjectRecordTotals.cs
@@ -73,7 +73,17 @@ namespace IG_CalculateProjectRecordTotals
                         {
                             EntityReference projectRecord = (EntityReference)actualCost.Attributes["ig1_projectrecord"];
                             QuickBooksTotals(projectRecord);
+                            TimeTraxCostsTotals(projectRecord);
                         } 
+                    }
+                    if (entity.LogicalName== "ig1_projectrecordhours")
+                    {
+                        Entity actulaHours = service.Retrieve(entity.LogicalName, entity.Id, new ColumnSet("ig1_projectrecord"));
+                        if (actulaHours.Attributes.Contains("ig1_projectrecord") && actulaHours.Attributes["ig1_projectrecord"] != null)
+                        {
+                            EntityReference projectRecord = (EntityReference)actulaHours.Attributes["ig1_projectrecord"];
+                            TimeTraxHoursTotls(projectRecord);
+                        }
                     }
                 }
                 else if (context.MessageName.ToLower() == "delete" && context.PreEntityImages.Contains("Image"))
@@ -123,7 +133,8 @@ namespace IG_CalculateProjectRecordTotals
                 }
             }
             catch (Exception ex)
-            { 
+            {
+                
             }
         }
         protected void CalculatePOTotals(EntityReference project)
@@ -335,7 +346,8 @@ namespace IG_CalculateProjectRecordTotals
         var fetchData = new
         {
             statecode = "0",
-            ig1_projectrecord = project.Id
+            ig1_projectrecord = project.Id,
+            ig1_amount = "0"
         };
         var fetchXml = $@"
                         <fetch>
@@ -347,6 +359,7 @@ namespace IG_CalculateProjectRecordTotals
                                 <condition attribute='ig1_txnid' operator='not-null' />
                                 <condition attribute='ig1_txnlineid' operator='not-null' />
                                 <condition attribute='ig1_projectrecord' operator='eq' value='{fetchData.ig1_projectrecord/*bd47d04f-cf32-ea11-a813-000d3a55d0f0*/}'/>
+                                <condition attribute='ig1_amount' operator='gt' value='{fetchData.ig1_amount/*0*/}'/>
                             </filter>
                             </entity>
                         </fetch>";
@@ -383,6 +396,133 @@ namespace IG_CalculateProjectRecordTotals
             entity.Attributes["ig1_pobills"] = new Money(PO_Bills);
             entity.Attributes["ig1_iginstallpobill"] = new Money(IG_Install_Po_Bills);
             entity.Attributes["ig1_travel"] = new Money(Travel);
+            service.Update(entity);
+        }
+        protected void TimeTraxCostsTotals(EntityReference project)
+        {
+            decimal salesCost = Convert.ToDecimal(0);
+            decimal designCost = Convert.ToDecimal(0);
+            decimal pmCost = Convert.ToDecimal(0);
+            decimal installerCost = Convert.ToDecimal(0);
+            var fetchData = new
+            {
+                statecode = "0",
+                ig1_projectrecord = project.Id,
+                ig1_amount = "0"
+            };
+            var fetchXml = $@"
+                            <fetch>
+                              <entity name='ig1_projectrecordcost'>
+                                <attribute name='ig1_expensetype' />
+                                <attribute name='ig1_amount' />
+                                <filter type='and'>
+                                  <condition attribute='statecode' operator='eq' value='{fetchData.statecode/*0*/}'/>
+                                  <condition attribute='ig1_projectrecord' operator='eq' value='{fetchData.ig1_projectrecord/*ig1_projectrecord*/}'/>
+                                  <condition attribute='ig1_amount' operator='gt' value='{fetchData.ig1_amount/*0*/}'/>
+                                  <condition attribute='ig1_txnid' operator='null' />
+                                  <condition attribute='ig1_txnlineid' operator='null' />
+                                </filter>
+                              </entity>
+                            </fetch>";
+            EntityCollection entityCollection = service.RetrieveMultiple(new FetchExpression(fetchXml));
+            if (entityCollection.Entities.Count > 0)
+            {
+                foreach (var item in entityCollection.Entities)
+                {
+                    if (item.Attributes.Contains("ig1_amount") && item.Attributes["ig1_amount"] != null)
+                    {
+                        Money money = (Money)item.Attributes["ig1_amount"];
+                        if (item.Attributes.Contains("ig1_expensetype") && item.Attributes["ig1_expensetype"] != null)
+                        {
+                            EntityReference type = (EntityReference)item.Attributes["ig1_expensetype"];
+                            if (type.Name == "Sales Cost")
+                            {
+                                salesCost += Convert.ToDecimal(money.Value);
+                            }
+                            else if (type.Name == "Design Cost")
+                            {
+                                designCost += Convert.ToDecimal(money.Value);
+                            }
+                            else if (type.Name == "PM Cost")
+                            {
+                                pmCost += Convert.ToDecimal(money.Value);
+                            }
+                            else if (type.Name == "Installer Cost")
+                            {
+                                installerCost += Convert.ToDecimal(money.Value);
+                            }
+                        }
+                    }
+                }
+            }
+            Entity entity = service.Retrieve(project.LogicalName, project.Id, new ColumnSet("ig1_projectrecordid"));
+            entity.Attributes["ig1_actualsalescost"] = new Money(salesCost);
+            entity.Attributes["ig1_actualdesigncost"] = new Money(designCost);
+            entity.Attributes["ig1_actualpmcost"] = new Money(pmCost);
+            entity.Attributes["ig1_installercost"] = new Money(installerCost);
+            service.Update(entity);
+        }
+        protected void TimeTraxHoursTotls(EntityReference project)
+        {
+            decimal salesHours = Convert.ToDecimal(0);
+            decimal designHours = Convert.ToDecimal(0);
+            decimal pmHours = Convert.ToDecimal(0);
+            decimal installerHours = Convert.ToDecimal(0);
+
+            var fetchData = new
+            {
+                statecode = "0",
+                ig1_projectrecord = project.Id,
+                ig1_duration = "0"
+            };
+            var fetchXml = $@"
+                            <fetch>
+                              <entity name='ig1_projectrecordhours'>
+                                <attribute name='ig1_duration' />
+                                <attribute name='ig1_type' />
+                                <filter type='and'>
+                                  <condition attribute='statecode' operator='eq' value='{fetchData.statecode/*0*/}'/>
+                                  <condition attribute='ig1_projectrecord' operator='eq' value='{fetchData.ig1_projectrecord/*ig1_projectrecord*/}'/>
+                                  <condition attribute='ig1_duration' operator='gt' value='{fetchData.ig1_duration/*0*/}'/>
+                                </filter>
+                              </entity>
+                            </fetch>";
+            EntityCollection entityCollection = service.RetrieveMultiple(new FetchExpression(fetchXml));
+            if (entityCollection.Entities.Count>0)
+            {
+                foreach (var item in entityCollection.Entities)
+                {
+                    if (item.Attributes.Contains("ig1_duration") && item.Attributes["ig1_duration"] != null)
+                    {
+                        decimal duration = Convert.ToDecimal(item.Attributes["ig1_duration"]);
+                        if (item.Attributes.Contains("ig1_type") && item.Attributes["ig1_type"] != null)
+                        {
+                            OptionSetValue type = (OptionSetValue)item.Attributes["ig1_type"];
+                            if (type.Value == Convert.ToInt32(0))
+                            {
+                                salesHours += duration;
+                            }
+                            else if (type.Value == Convert.ToInt32(1))
+                            {
+                                designHours += duration;
+                            }
+                            else if (type.Value == Convert.ToInt32(2))
+                            {
+                                pmHours += duration;
+                            }
+                            else if (type.Value == Convert.ToInt32(3))
+                            {
+                                installerHours += duration;
+                            }
+                        }
+                    }
+                }
+            }
+            Entity entity = service.Retrieve(project.LogicalName, project.Id, new ColumnSet("ig1_projectrecordid"));
+            entity.Attributes["ig1_saleshours"] = salesHours;
+            entity.Attributes["ig1_actualdesignhours"] = designHours;
+            entity.Attributes["ig1_actualpmhours"] = pmHours;
+            entity.Attributes["ig1_installerhours"] = installerHours;
             service.Update(entity);
         }
     }
