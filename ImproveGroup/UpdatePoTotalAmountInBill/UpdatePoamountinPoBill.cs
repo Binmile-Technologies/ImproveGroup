765b5cb15232522ef468ce814cf022b045edb0e9
snehbinmile <65010773+snehbinmile@users.noreply.github.com>
2021-01-28 11:53:48 +0530

    IG-179
    For Update Purchase Order Total amount in  POBIll.

---
+++ ImproveGroup/UpdatePoTotalAmountInBill/UpdatePoamountinPoBill.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdatePoTotalAmountInBill
+{
+    public class UpdatePoamountinPoBill:IPlugin
+    {
+        IPluginExecutionContext context;
+        IOrganizationServiceFactory serviceFactory;
+        IOrganizationService service;
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            service = serviceFactory.CreateOrganizationService(null);
+
+
+            try
+            {
+                if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+                {
+
+                    Entity entity = (Entity)context.InputParameters["Target"];
+
+                    if (entity.LogicalName == "msdyn_purchaseorderbill")
+                    {
+                        Entity poentity = service.Retrieve(entity.LogicalName, entity.Id, new ColumnSet("msdyn_purchaseorder"));
+                        Guid poid = poentity.GetAttributeValue<EntityReference>("msdyn_purchaseorder").Id;
+                        Money amount   =  GetPOtotalamount(poid);
+                        entity["ig1_pototalamount"] = amount.Value;
+                        service.Update(entity);
+
+                    }
+
+                    else if(entity.LogicalName == "msdyn_purchaseorder")
+                    {
+
+                        Entity pobil=service.Retrieve(entity.LogicalName, entity.Id, new ColumnSet("msdyn_totalamount"));
+                        Money money=pobil.GetAttributeValue<Money>("msdyn_totalamount");
+                        UpdatePOBillAmount(entity.Id, money);
+
+
+                    }
+
+
+                }
+
+
+            }
+
+
+
+            catch(Exception ex)
+            {
+
+                throw ex;
+            }
+
+            
+        }
+
+        public void UpdatePOBillAmount(Guid poid, Money amount)
+        {
+            var fetchData = new
+            {
+                msdyn_purchaseorder = poid,
+                statecode = "0"
+            };
+            var fetchXml = $@"
+                    <fetch>
+                      <entity name='msdyn_purchaseorderbill'>
+                        <attribute name='msdyn_purchaseorderbillid' />
+                        <attribute name='ig1_pototalamount' />
+                        <filter>
+                          <condition attribute='msdyn_purchaseorder' operator='eq' value='{fetchData.msdyn_purchaseorder/*af5f28e4-918d-4889-9d98-e9024b9e37f1*/}'/>
+                          <condition attribute='statecode' operator='eq' value='{fetchData.statecode/*0*/}'/>
+                        </filter>
+                      </entity>
+                    </fetch>";
+
+            EntityCollection result = service.RetrieveMultiple(new FetchExpression(fetchXml));
+
+
+             if ( result.Entities.Count > 0 && result.Entities != null )
+            {
+
+                for (int i = 0; i < result.Entities.Count; i++)
+                {
+                    Guid id = result.Entities[i].GetAttributeValue<Guid>("msdyn_purchaseorderbillid");
+                    Entity pobil = new Entity("msdyn_purchaseorderbill", id);
+                    pobil.Attributes["ig1_pototalamount"] = amount.Value;
+                    service.Update(pobil);
+
+                }
+                                                
+            }
+
+        }
+
+        public Money GetPOtotalamount(Guid Id)
+        {
+            Money pototal = new Money(0);
+
+            var fetchData = new
+            {
+                msdyn_purchaseorderid = Id
+            };
+            var fetchXml = $@"
+                <fetch>
+                  <entity name='msdyn_purchaseorder'>
+                    <attribute name='msdyn_totalamount' />
+                    <filter>
+                      <condition attribute='msdyn_purchaseorderid' operator='eq' value='{fetchData.msdyn_purchaseorderid/*f48d50db-1f6e-4397-8694-18fac4664b4b*/}'/>
+                    </filter>
+                  </entity>
+                </fetch>";
+              EntityCollection result = service.RetrieveMultiple(new FetchExpression(fetchXml));
+
+            if (result.Entities[0].Attributes.Contains("msdyn_totalamount") && result.Entities[0].Attributes["msdyn_totalamount"] !=null)
+            {
+
+                pototal = result.Entities[0].GetAttributeValue<Money>("msdyn_totalamount");
+
+            }
+
+
+            return pototal;
+        }
+
+
+    }
+}
